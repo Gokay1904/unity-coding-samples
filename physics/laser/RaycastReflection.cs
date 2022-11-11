a8bbb3bc69227d61a591dd595da2f8fd0764d47d
Gökay Akçay <Gokay1904@gmail.com>
2022-11-11 18:25:32 +0300

    Create RaycastReflection.cs

---
+++ physics/laser/RaycastReflection.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(LineRenderer))]
+public class RaycastReflection : MonoBehaviour
+{
+    public int reflections;
+    public float maxLength;
+
+    private LineRenderer lineRenderer;
+    private Ray ray;
+    private RaycastHit2D hit;
+    private Vector3 direction;
+
+
+
+    private void Awake()
+    {
+     
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+
+        ReflectionFor2D();
+    }
+    void ReflectionFor2D()
+    {
+        ray = new Ray(transform.position, transform.right);
+
+        lineRenderer.positionCount = 1;
+
+        lineRenderer.SetPosition(0, transform.position);
+
+        float remainingLength = maxLength;
+
+        for (int i = 0; i < reflections; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, remainingLength, LayerMask.GetMask("Reflective"));
+            if (hit)
+            {
+                if (hit.collider.tag == "Reflectable") 
+                {
+
+                lineRenderer.positionCount += 1;
+
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
+
+                remainingLength -= Vector3.Distance(ray.origin, hit.point);
+
+                ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+                   
+                    if (hit.collider.GetComponent<DefaultBlock>() != null)
+                    {
+                        if(hit.collider.GetComponent<DefaultBlock>().isRayInteracted == false) 
+                        {
+                        hit.collider.GetComponent<DefaultBlock>().isRayInteracted = true;
+                        DefaultBlock hittedBlock = hit.collider.GetComponent<DefaultBlock>();
+
+                        GameManagement.gameManagement.activatedDefaultBlocks.Add(hittedBlock);          
+                        }     
+                    } 
+                }
+
+                if (hit.collider.tag != "Reflectable")
+                {
+                   
+                    break;
+                }
+ 
+            }
+             
+            else
+            {        
+                lineRenderer.positionCount += 1;
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, ray.origin + ray.direction * remainingLength);
+            }
+
+        }
+    }
+
+}
